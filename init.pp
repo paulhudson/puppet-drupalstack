a9619666c745e13432923bf8886806c18e9c0470
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-05-22 19:25:50 +1000

    ensure /etc/php.d

--- init.pp
+++ init.pp
@@ -34,11 +34,8 @@ package { 'ImageMagick':
 $phpModules = [ 'curl', 'mysql', 'cli', 'intl', 'mcrypt', 'memcache', 'gd', 'apc']
 php::module { $phpModules: }
 
-file { [ "/etc/php.d/conf.d", "/etc/php.d/cli", "/etc/php.d/cli/conf.d"]:
+file { [ "/etc/php.d", "/etc/php.d/conf.d", "/etc/php.d/cli", "/etc/php.d/cli/conf.d"]:
   ensure => "directory",
-  owner => 'apache',
-  group => 'apache',
-  mode => 750,
 }
 
 php::ini { 'php':
