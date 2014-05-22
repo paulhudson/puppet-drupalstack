5aa62713fed464ecdc2c69298d217e2b20167d88
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-05-22 17:59:45 +1000

    Add apache and php directives

--- init.pp
+++ init.pp
@@ -1,5 +1,26 @@
+ # Apache
  class { 'apache':}
+ 
+ apache::module { 'rewrite': }
 
- apache::vhost { 'mysite':
-   docroot  => '/var/www/vhosts',
- }
+ include apache::ssl
+ 
+ apache::vhost { 'drupal':
+   docroot  => '/var/www/vhosts/drupal',
+   docroot_create => true,
+   directory_allow_override   => 'All',
+   ssl => true,
+   server_name => 'drupal',
+}
+
+# PHP
+class { 'php': }
+
+$phpModules = [ 'imagick', 'xdebug', 'curl', 'mysql', 'cli', 'intl', 'mcrypt', 'memcache', 'gd', 'apc']
+php::module { $phpModules: }
+
+php::ini { 'php':
+  value   => ['memory_limit = "256M"'],
+  target  => 'php.ini',
+  service => 'apache',
+}
