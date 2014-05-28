73b2fd48ee17a55cce935cc3bc5b5050aedac4c1
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-05-28 23:56:21 +1000

    debug

--- init.pp
+++ init.pp
@@ -105,8 +105,8 @@ augeas { 'sudoapache':
         'set spec[user = "%apache"]/host_group/command /root/puppet-drupalstack/shell/vhost_deploy.sh',
         'set spec[user = "%apache"]/host_group/command/runas_user ALL',
          # Don't require tty
-        'set Defaults:apache    !requiretty',
-        'set Defaults:apache    !visiblepw',
+        #'set Defaults:apache    !requiretty',
+        #'set Defaults:apache    !visiblepw',
         
     ]
 }
