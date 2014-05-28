28e2363eaf669386ca0de6e02eb9fa4d4a9af4cf
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-05-28 16:14:10 +1000

    allow install of Drupal distributions

--- shell/vhost_deploy.sh
+++ shell/vhost_deploy.sh
@@ -1,9 +1,11 @@
 aliasflag=''
 helpflag='false'
+distribution='drupal'
 
-while getopts 'a:h' flag; do
+while getopts 'a:h:d' flag; do
 case "${flag}" in
 a) aliasflag="${OPTARG}" ;;
+d) distribution="${OPTARG}" ;;
 h) helpflag='true' ;;
 *) error "Unexpected option ${flag}" ;;
 esac
@@ -15,7 +17,8 @@ echo "Add new vhost and Drupal install"
 echo " "
 echo "options:"
 echo "-h, --help                show this help"
-echo "-a                        alias hostname for the new site, e.g. drupal.test"
+echo "-a                        alias - hostname for the new site, e.g. drupal.test"
+echo "-d                        distribution - the Drupal distribution you want to install (default: drupal)"
 exit 0
 fi
 
@@ -31,9 +34,9 @@ db_user=$(echo $aliasflag | tr -dc _A-Z-a-z-0-9 | head -c6 ;)
 FACTER_sitename=$aliasflag FACTER_db_user=$db_user FACTER_db_pass=$db_pass puppet apply site.pp
 
 cd /var/www/vhosts/$aliasflag
-drush dl drupal --drupal-project-rename=$db_user
+drush dl $distribution --drupal-project-rename=$db_user
 chown -R apache:apache ./$db_user && mv ./$db_user/* ./ && rm -rf ./$db_user
-drush site-install standard --db-url=mysql://$db_user:$db_pass@localhost/$db_user --site-name=$db_user -y
+drush site-install $distribution --db-url=mysql://$db_user:$db_pass@localhost/$db_user --site-name=$aliasflag -y
 
 
 # set PATH or bash_profile alias, etc for vhosts/drupal installer sh
