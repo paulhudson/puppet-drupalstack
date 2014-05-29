7f29d3501e0612f840db7e7a01ebad190b23252a
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-05-29 12:45:49 +1000

    debugging

--- files/drupal-install/index.php
+++ files/drupal-install/index.php
@@ -114,8 +114,9 @@
         $distribution = escapeshellarg($_GET['exclude_paths']);
         #$command = escapeshellcmd("sudo /root/puppet-drupalstack/lib/vhost_deploy.sh -a $domain -d $distribution > /dev/null 2>/dev/null &");
         $command = escapeshellcmd("sudo /root/puppet-drupalstack/lib/vhost_deploy.sh -a $domain -d $distribution > /dev/null 2> /dev/null &");
-        #$output = shell_exec($command);
-        exec($command);
+        $output = shell_exec($command);
+        #exec($command);
+
 
         // Write to config
         $conf_file = '/var/log/drupal-install';
