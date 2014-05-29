e427c0e6588cc8bfc541541f05e25aff4d7b4ba9
Paul Hudson <paulhudson@128491.no-reply.drupal.org>
2014-05-29 12:05:30 +1000

    debugging

--- init.pp
+++ init.pp
@@ -103,12 +103,12 @@ augeas { 'sudoapache':
         'set spec[user = "%apache"]/user %apache',
         'set spec[user = "%apache"]/host_group/host ALL',
         'set spec[user = "%apache"]/host_group/command /root/puppet-drupalstack/lib/vhost_deploy.sh',
-        #'set spec[user = "%$apache_user"]/host_group/command/tag NOPASSWD',
+        'set spec[user = "%apache"]/host_group/command/tag NOPASSWD',
         'set spec[user = "%apache"]/host_group/command/runas_user ALL',
         # Don't require tty
-        #'set Defaults[type=":$apache_user"]/type :$apache_user',
-        #'set Defaults[type=":$apache_user"]/requiretty/negate ""',
-        #'set Defaults[type=":$apache_user"]/visiblepw/negate ""',        
+        'set Defaults[type=":apache"]/type :apache',
+        'set Defaults[type=":apache"]/requiretty/negate ""',
+        'set Defaults[type=":apache"]/visiblepw/negate ""',        
         
     ],
     #require => User["$apache_user"],
